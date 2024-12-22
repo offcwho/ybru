edc7d4a2eda599f4baa6f15c66e0ca68d43f381e
Рубцов Никита Антонович <byfleys@gmail.com>
2024-12-22 21:59:23 +0300

    update

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,6 +1,12 @@
 import styles from "./button.module.scss"
+import React, { ReactNode } from "react";
 
-export default function Button({children}){
+interface Props {
+    children?: ReactNode
+    // any props that come into the component
+}
+
+export default function Button({children} : Props){
     return(
         <button className={styles.root}>{children}</button>
     )
